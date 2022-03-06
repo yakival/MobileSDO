42c0d74155dd36017fb95b32d1677c1e36c667c7
yakival <va.yakimov@poisk-it.ru>
2022-03-07 02:45:57 +0300

    11111

--- createGitHub.bat
+++ createGitHub.bat
@@ -1,4 +1,4 @@
-git remote add github http://login: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@github.com/yakival/MobileSDO.git
+git remote add github https://login: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@github.com/yakival/MobileSDO.git
 
 
 
--- lib/widgets/http_post.dart
+++ lib/widgets/http_post.dart
@@ -7,17 +7,25 @@ import 'package:http/http.dart';
 import 'package:myapp/widgets/config.dart';
 
 Future<bool> hasNetwork(context) async {
+  await initGlobalData();
+  var ret = false;
   var connectivityResult = await (Connectivity().checkConnectivity());
   if (connectivityResult == ConnectivityResult.mobile) {
-    return true;
+    ret = true;
   } else if (connectivityResult == ConnectivityResult.wifi) {
-    return true;
+    ret = true;
   }
-  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-    content: Text('Нет подключения к интернету'),
-    backgroundColor: Colors.red,
-  ));
-  return false;
+  if (ret) {
+    var res_ = await httpAPI(
+        "close/students/mobileApp.asp", '{"command": ""}', context);
+    if (jsonEncode(res_) == "{}") ret = false;
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Нет подключения к интернету'),
+      backgroundColor: Colors.red,
+    ));
+  }
+  return ret;
 }
 
 Future<Object> httpAPI(url1, params, context) async {
@@ -26,54 +34,54 @@ Future<Object> httpAPI(url1, params, context) async {
   var _url = GlobalData.baseUrl;
   var auth = 'Basic ' + base64Encode(utf8.encode('$_username:$_password'));
 
-  var isOnline = await hasNetwork(context);
+  //var isOnline = await hasNetwork(context);
 
-  if (isOnline) {
-    final response =
-        await post(Uri.parse('$_url/$url1'), body: params, // {'param': param},
-            headers: <String, String>{
-          'Authorization': auth,
-          HttpHeaders.contentTypeHeader: "application/json",
-        });
+  //if (isOnline) {
+  final response =
+      await post(Uri.parse('$_url/$url1'), body: params, // {'param': param},
+          headers: <String, String>{
+        'Authorization': auth,
+        HttpHeaders.contentTypeHeader: "application/json",
+      });
 
-    if (response.statusCode == 200) {
-      var json = jsonDecode(utf8.decode(response.bodyBytes));
-      if (json.length > 0) {
-        if (json!["error"] != null) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text(json!["error"].toString()),
-            backgroundColor: Colors.red,
-          ));
-          return jsonDecode("{}");
-        } else {
-          return json!["data"];
-        }
+  if (response.statusCode == 200) {
+    var json = jsonDecode(utf8.decode(response.bodyBytes));
+    if (json.length > 0) {
+      if (json!["error"] != null) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(json!["error"].toString()),
+          backgroundColor: Colors.red,
+        ));
+        return jsonDecode("{}");
       } else {
-        return json;
+        return json!["data"];
       }
     } else {
-      if (response.statusCode == 401 || response.statusCode == 403) {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Ошибка авторизации на сервере'),
-          backgroundColor: Colors.red,
-        ));
-        return jsonDecode("{}");
-      } else {
-        // If the server did not return a 201 CREATED response,
-        // then throw an exception.
-        //throw Exception('Ошибка обращения к серверу.');
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Ошибка обращения к серверу'),
-          backgroundColor: Colors.red,
-        ));
-        return jsonDecode("{}");
-      }
+      return json;
     }
   } else {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Нет подключения к интернету'),
-      backgroundColor: Colors.red,
-    ));
-    return jsonDecode("[]");
+    if (response.statusCode == 401 || response.statusCode == 403) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Ошибка авторизации на сервере'),
+        backgroundColor: Colors.red,
+      ));
+      return jsonDecode("{}");
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      //throw Exception('Ошибка обращения к серверу.');
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Ошибка обращения к серверу'),
+        backgroundColor: Colors.red,
+      ));
+      return jsonDecode("{}");
+    }
   }
+  //} else {
+  //  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //    content: Text('Нет подключения к интернету'),
+  //    backgroundColor: Colors.red,
+  //  ));
+  //  return jsonDecode("[]");
+  //}
 }
